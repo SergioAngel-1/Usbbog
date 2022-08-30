0d7385abab11c5836d2f643954e273cd81d6350b
SergioAngel-1 <sergioja2003@gmail.com>
2022-08-30 15:18:36 -0500

    body v0.2

---
+++ script.js
@@ -0,0 +1,11 @@
+function showText() {
+  document.getElementById("spanHideText").style.display = "block";
+  document.getElementById("seeLess").style.display = "block";
+  document.getElementById("seeMore").style.display = "none";
+}
+
+function hideText() {
+  document.getElementById("spanHideText").style.display = "none";
+  document.getElementById("seeLess").style.display = "none";
+  document.getElementById("seeMore").style.display = "block";
+}
